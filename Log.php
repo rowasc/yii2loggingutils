03b86fbd250c7dda8174f9fe082589bb81ca9499
Romina Suarez <rowasc@gmail.com>
2015-09-03 00:02:35 -0300

    Not tested - new refactored  version

--- Log.php
+++ Log.php
@@ -9,24 +9,53 @@
 namespace rowasc\components;
 use \Yii;
 
+/**
+ * Class Log
+ * @author rowasc
+ * @package rowasc\components
+ */
 trait Log
 {
 
-    public function _logError($message) {
-        Yii::error($message, $this->log_category);
+    /**
+     * @param      $message
+     * @param null $log_category_override
+     */
+    public function _logInfo($message,$log_category_override=null) {
+        Yii::trace(var_export($message,true), $this->_getLogCategory($log_category_override));
+    }
+
+    /**
+     * @param      $variable
+     * @param null $log_category_override
+     */
+    public function _logTrace($variable,$log_category_override=null) {
+        Yii::info($variable, $this->_getLogCategory($log_category_override));
+    }
+
+    /**
+     * @param      $message
+     * @param null $log_category_override
+     */
+    public function _logError($message,$log_category_override=null) {
+        Yii::error($message, $this->_getLogCategory($log_category_override));
 
     }
 
-    public function _logInfo($message) {
-        Yii::trace(var_export($message,true), $this->log_category);
+    /**
+     * @param        $message
+     * @param string $log_category_override
+     */
+    public function _logWarning($message, $log_category_override=null) {
+        Yii::warning($message, $this->_getLogCategory($log_category_override));
     }
 
-    public function _logTrace($variable) {
-        Yii::info($variable, $this->log_category);
+    /**
+     * @param $log_category_override
+     *
+     * @return string
+     */
+    protected function _getLogCategory($log_category_override){
+        return $log_category_override!==null?$log_category_override:$this->log_category;
     }
-
-    public function _logWarning($message) {
-        Yii::warning($message, $this->log_category);
-    }
-
 }
